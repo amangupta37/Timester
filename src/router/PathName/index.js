a1341e1e9db1b6d9f31d87e1706b38ba08b59518
Aman Gupta <amangupta6121998@gmail.com>
2023-05-22 21:22:47 +0530

    feat(router/pathname): add pathname variable for routes

---
+++ src/router/PathName/index.js
@@ -0,0 +1,11 @@
+export const PathName = {
+    LANDING: "/",
+    LOGIN: "/login",
+    SIGNUP: "/signup",
+    PRODUCT: "/product",
+    CART: "/cart",
+    PRODUCT_LISTING: "/product-listing",
+    WISHLIST: "/wishlist",
+    GENERIC_COMPONENTS: "/comp",
+    PAGE_NOT_FOUND: "*",
+};
