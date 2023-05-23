3dcc8349a2bd4f137435cac92be0411ec06f54bd
Aman Gupta <amangupta6121998@gmail.com>
2023-05-23 22:14:06 +0530

    feat(App.jsx): add lazy-loading and routing logic with suspense fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./router";
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Timester</h1>
-        </div>
+        <Suspense fallback={<h1>Loading....</h1>}>
+            <Router>
+                <Routes>
+                    {routes?.map((route) => (
+                        <Route
+                            key={route?.key}
+                            path={route?.path}
+                            element={route?.component}
+                        />
+                    ))}
+                </Routes>
+            </Router>
+        </Suspense>
     );
 };
 
