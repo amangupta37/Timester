4e008aab4b3bf2187c25794bdd1a0deb38174069
Aman Gupta <amangupta6121998@gmail.com>
2023-05-23 21:56:11 +0530

    chore(pages/index): add dynamic path import-export for pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Cart from "./Cart/Cart";
+import Landing from "./Landing/Landing";
+import Login from "./Login/Login";
+import Product from "./Product/Product";
+import ProductListing from "./ProductListing/ProductListing";
+import Signup from "./Signup/Signup";
+import Wishlist from "./Wishlist/Wishlist";
+
+export { Cart, Landing, Login, Product, ProductListing, Signup, Wishlist };
