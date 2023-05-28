e3f65e797b974e41e6b0564ed214780f568a89dd
Aman Gupta <amangupta6121998@gmail.com>
2023-05-28 07:42:02 +0530

    chore(assets/svg/index): add file import-export for svg  icons

---
+++ src/assets/svg/index.js
@@ -0,0 +1,5 @@
+import Github from "./social/github/Github";
+import LinkedIn from "./social/linkedIn/LinkedIn";
+import Medium from "./social/medium/Medium";
+import Twitter from "./social/twitter/Twitter";
+export { Github, LinkedIn, Medium, Twitter };
