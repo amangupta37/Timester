d272fa468e03662231eaea97ea92cf5c726e10a6
Aman Gupta <amangupta6121998@gmail.com>
2023-05-30 20:49:37 +0530

    refactor(pages/landing): updated layout for landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Landing.css";
 import { LandingLayout } from "../../layouts";
+import { BestSellers, BrandCategory } from "../../components";
 const Landing = () => {
-    return <LandingLayout></LandingLayout>;
+    return (
+        <LandingLayout>
+            <BrandCategory />
+            <BestSellers />
+        </LandingLayout>
+    );
 };
 
 export default Landing;
