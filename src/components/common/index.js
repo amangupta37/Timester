c806ae399e216709cbcd95891e4a6461e55d9afa
Aman Gupta <amangupta6121998@gmail.com>
2023-05-31 20:56:27 +0530

    feat(components/common/index): add path import-export for all common components

---
+++ src/components/common/index.js
@@ -0,0 +1,21 @@
+import Banner from "./Banner/Banner";
+import Button from "./Button/Button";
+import Card from "./Card/Card";
+import Input from "./Input/Input";
+import Logo from "./Logo/Logo";
+import Navbar from "./Navbar/Navbar";
+import Notification from "./Notification/Notification";
+import SearchBar from "./SearchBar/SearchBar";
+import Typography from "./Typography/Typography";
+
+export {
+    Banner,
+    Button,
+    Card,
+    Input,
+    Logo,
+    Navbar,
+    Notification,
+    SearchBar,
+    Typography,
+};
