826dca562b66c902cd313783c86c158c009c1cb9
Aman Gupta <amangupta6121998@gmail.com>
2023-05-31 20:54:35 +0530

    refactor(components/common/logo): add semantic tag as image wrapper and src name update

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Typography from "../Typography/Typography";
-import { watch } from "../../../assets/img";
+import { Watch } from "../../../assets/img";
 import "./Logo.css";
 const Logo = () => {
     return (
         <div className="Logo">
-            <div className="Logo__Icon">
-                <img src={watch} alt="brand-logo" />
-            </div>
+            <figure className="Logo__Icon">
+                <img src={Watch} alt="brand-logo" />
+            </figure>
             <Typography className="Logo__BrandName" variant="h1">
                 Timester
             </Typography>
