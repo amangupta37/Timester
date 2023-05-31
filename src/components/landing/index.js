4441aeb917889290ea16c4064afe25735245f98a
Aman Gupta <amangupta6121998@gmail.com>
2023-05-31 20:55:36 +0530

    feat(components/landing/index): add path import-export for all landing components

---
+++ src/components/landing/index.js
@@ -0,0 +1,15 @@
+import BestSellers from "./BestSellers/BestSellers";
+import BrandCard from "./BrandCard/BrandCard";
+import BrandCategory from "./BrandCategory/BrandCategory";
+import HeroSection from "./HeroSection/HeroSection";
+import ProductCard from "./ProductCard/ProductCard";
+import Section from "./Section/Section";
+
+export {
+    BestSellers,
+    BrandCard,
+    BrandCategory,
+    HeroSection,
+    ProductCard,
+    Section,
+};
