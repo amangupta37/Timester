ec84fec9fbe48bc73995733e79bc986dabfa463d
Aman Gupta <amangupta6121998@gmail.com>
2023-05-31 20:56:53 +0530

    feat(components/components/index): add path import-export for all components

---
+++ src/components/index.js
@@ -0,0 +1,49 @@
+import {
+    Banner,
+    Button,
+    Card,
+    Input,
+    Logo,
+    Navbar,
+    Notification,
+    SearchBar,
+    Typography,
+} from "./common";
+
+import { CartIcon, CircleOverlapIcon, WishlistIcon } from "./icons";
+
+import {
+    BrandCategory,
+    BestSellers,
+    HeroSection,
+    ProductCard,
+    Section,
+} from "./landing";
+
+import { Header, Footer } from "./layout";
+
+import { ProductBannerSection, SideMenu } from "./products";
+
+export {
+    Banner,
+    BestSellers,
+    Button,
+    BrandCategory,
+    Card,
+    CartIcon,
+    CircleOverlapIcon,
+    Footer,
+    Input,
+    Logo,
+    Navbar,
+    Notification,
+    ProductBannerSection,
+    ProductCard,
+    SearchBar,
+    Section,
+    SideMenu,
+    Typography,
+    Header,
+    HeroSection,
+    WishlistIcon,
+};
