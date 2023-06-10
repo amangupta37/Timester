ee7556fb72981e4c69b38c4285e594198ce7dd39
Aman Gupta <amangupta6121998@gmail.com>
2023-06-10 22:10:37 +0530

    feat(pages/product): add product page component

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { SideMenu } from "../../components";
+import { ProductLayout } from "../../layouts";
 import "./Product.css";
+
 const Product = () => {
-    return <div>Product</div>;
+    return (
+        <ProductLayout>
+            <aside className="Product__Aside">
+                <SideMenu />
+            </aside>
+
+            <section className="Product__Section"></section>
+        </ProductLayout>
+    );
 };
 
 export default Product;
